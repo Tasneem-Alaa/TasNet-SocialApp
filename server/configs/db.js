63fb65076620a4d2328c67bb0d6d830fd7288793
Tasneem Alaa <tasnemalaa30@gmail.com>
2026-01-18 22:26:01 +0200

    sync data opertaions with clerk

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', () => console.log('MONGODB CONNECTED SUCCESFULLY✅'))
-        await mongoose.connect(`${process.env.MOGODB_URL}/TasNet`)
+        await mongoose.connect(`${process.env.MONGODB_URL}/TasNet`)
     } catch (error) {
         console.log("Error connecting to MONGODB❌",error.message);
         process.exit(1); //exit with failure, and 0 means exit with succes
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.js'
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express()
 
@@ -10,9 +11,8 @@ await connectDB()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req,res)=>{
-    res.send('Server is runing hehe')
-})
+app.get('/', (req,res)=>res.send('Server is runing hehe'))
+app.use('/api/inngest',serve({ client: inngest, functions }))
 
 const PORT = process.env.PORT || 4000
 
