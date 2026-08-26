d7c707b1442b481c301ddc7c59f041f5160e38b7
Tasneem Alaa <tasnemalaa30@gmail.com>
2026-08-26 19:34:23 +0300

    fix: update default display values for message and share counts in PostCard component

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -95,11 +95,11 @@ const PostCard = ({post}) => {
             </div>
             <div className='flex items-center gap-1'>
                 <MessageCircle className='w-4 h-4'/>
-                <span>{12}</span>
+                <span>{0}</span>
             </div>
             <div className='flex items-center gap-1'>
                 <Share2 className='w-4 h-4'/>
-                <span>{12}</span>
+                <span>{0}</span>
             </div>
 
         </div>
